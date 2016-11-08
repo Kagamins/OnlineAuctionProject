29366b01f9da32153308e4f78b074e746beb7e56
Emperor <nsoulper@gmail.com>
2016-11-08 03:58:36 +0300

    m,
    kuhn

---
+++ Users/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import User
+class SignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = '__all__'
+        widgets = {
+        'user' : forms.HiddenInput,
+        }
+    # university_id = forms.CharField(required=False)
+    # twitter_id = forms.CharField(required=False)
+
+    def signup(self, request, user):
+        Student.objects.create(
+            user=user,
+            name=user.username,
+            email=user.email,
+            university_id=self.cleaned_data.get('university_id'),
+            twitter_id=self.cleaned_data.get('twitter_id')
+        )
