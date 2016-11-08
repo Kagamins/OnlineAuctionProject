7a7dd4badf66fe7635b6417faddb31cf8cf178f4
Emperor <nsoulper@gmail.com>
2016-11-08 21:29:09 +0300

    1st template

--- Auction/models.py
+++ Auction/models.py
@@ -1,1 +1,25 @@
 from django.db import models
+import os
+
+
+def get_image_path(instance, filename):
+    return os.path.join('photos', str(instance.id), filename)
+
+
+class item(models.Model):
+    product_name = models.CharField(max_length=18,help_text=('Car_Name'))
+    manufacture_year = models.CharField(
+        max_length=120, help_text=('E.g.: 2015/2016'))
+
+    picture = models.FileField()
+    product_description = models.TextField()
+    def get_absolute_url(self):
+        return reverse(
+            'Create_Auction',
+            kwargs={'user': self.pk})
+
+
+class auction(models.Model):
+    product = models.ForeignKey(item)
+    auction_date = models.DateField()
+    auction_time = models.TimeField()
--- Users/models.py
+++ Users/models.py
@@ -6,14 +6,12 @@ from django.conf import settings
 
 class User(models.Model):
     name = models.CharField(max_length=120)
-    Id = models.BigIntegerField(null=True, blank=True)
     email = models.EmailField()
     Phone_num = models.IntegerField(null=True,blank=True)
     twitter_id = models.CharField(max_length=50, null=True, blank=True, default="")
     user = models.OneToOneField(settings.AUTH_USER_MODEL, null=True, blank=True)
 
-    def get_absolute_url(self):
-        return reverse('student_view', kwargs={'pk': self.pk})
+
 
     def __unicode__(self):
         return u"{} : {}".format(self.name, self.university_id)
