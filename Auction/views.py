14cfc9480d48a25b89d2c327578e5370f7daa86f
Emperor <nsoulper@gmail.com>
2016-11-12 16:36:45 +0300

    s

--- Auction/views.py
+++ Auction/views.py
@@ -1,1 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.http import Http404
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from django.views.generic.edit import UpdateView
+from django.views.generic import DetailView
+from django.db.models import Avg, Max, Min
+from .forms import newAuctionForm
+from .models import auction
+
+
+@login_required
+def createAuction(request):
+
+    if request.method == 'POST':
+        form = newAuctionForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save()
+            return redirect(obj)
+    else:
+
+        form = newAuctionForm(initial={'user': request.user.pk,
+                                       })
+
+    return render(
+        request,
+        'createAuction.html',
+        {
+            "form": form,
+        }
+    )
+
+
+class Create_Auction(CreateView):
+    model = auction
+    # we require all fields since students will need register him/her self
+    fields = '__all__'
+    template_name = 'createAuction.html'
+
+class Edit_Auction(UpdateView):
+    model = auction
+    fields = '__all__'
+    context_object_name = 'auction'
+    template_name = 'editAuction.html'
