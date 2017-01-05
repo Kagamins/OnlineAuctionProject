c0ec71008dce15ffebc98a06019d17fd36a939df
Emperor <nsoulper@gmail.com>
2017-01-05 12:41:56 +0300

    Finalize

--- OnlineAuctionProject/urls.py
+++ OnlineAuctionProject/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from Auction import views
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
@@ -10,5 +12,5 @@ urlpatterns = [
     url(r'^auction/',include('Auction.urls',)),
     url(r'^User/',include('Users.urls',)),
     url(r'^Support/',include('Support.urls',)),
-    url(r'^',views.index_page,name='home',),
-]
+    url(r'^$',views.index_page,name='home',),
+] + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
